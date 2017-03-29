548c7af35b8a419a00d6a2ffaf2a9cc179ff7b26
Sheprut Ilya <optitel223@gmail.com>
2017-03-29 22:55:06 +0700

    Написана реализация point

--- point.h
+++ point.h
@@ -2,31 +2,38 @@
 #define __POINTDEFINE
 
 #include <vector>
+#include <iostream>
+#include <string>
+#include <math.h>
 
 using namespace std;
 
 template<class T, int n = 2> class AnyPoint {
 	vector<T> a_;
 public:
-	AnyPoint(int = 0, int = 0, int = 0, int = 0);
-	~AnyPoint();
-	
+	AnyPoint(T = 0, T = 0, T = 0, T = 0);
 	T& operator[](int);
-	
-	T lenght(void);
-	
-	AnyPoint operator+(AnyPoint b);
-	bool operator!=(AnyPoint b);
-	bool operator==(AnyPoint b);
-	
-	AnyPoint operator-();
+	T length(void);
 };
+
+template<class T, int n> 
+AnyPoint<T, n> operator+( AnyPoint<T, n> a,  AnyPoint<T, n> b);
+template<class T, int n> 
+AnyPoint<T, n> operator-( AnyPoint<T, n> a,  AnyPoint<T, n> b);
+template<class T, int n> 
+AnyPoint<T, n> operator*( AnyPoint<T, n> a,  T b);
+template<class T, int n> 
+bool operator!=( AnyPoint<T, n> a,  AnyPoint<T, n> b);
+template<class T, int n> 
+bool operator==( AnyPoint<T, n> a,  AnyPoint<T, n> b);
+template<class T, int n> 
+ostream& operator<<(ostream& cout,  AnyPoint<T, n> a);
+template<class T, int n> 
+AnyPoint<T, n> operator-( AnyPoint<T, n> a);
 
 typedef AnyPoint<int,2> Point;
 typedef AnyPoint<double,2> point2;
 typedef AnyPoint<double,3> point3;
 typedef AnyPoint<double,4> point4;
-
-template<class T, int n> AnyPoint<T, n> operator*(AnyPoint<T, n>, double);
 
 #endif
